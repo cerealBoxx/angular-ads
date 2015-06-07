01aa34b3534a32d54ee2e5e76bf5d75442af1763
Tsveta Brakalova <cookie.bra@gmail.com>
2015-06-07 18:33:22 +0300

    Added ads state to routing.

--- src/app/index.js
+++ src/app/index.js
@@ -8,6 +8,10 @@ angular.module('ads', ['ngCookies', 'ngResource', 'ui.router', 'ui.bootstrap'])
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       })
+      .state('ads', {
+        templateUrl:'app/ads/ads-main.html',
+        controller: 'AdsMainCtrl'
+      })
       //.state('ad-details', {
       //  temlateUrl: 'app/ads/ad-details.html',
       //  controller: 'AdDetailsCtrl'
