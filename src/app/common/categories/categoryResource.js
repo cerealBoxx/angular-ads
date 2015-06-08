cd41cb99e0eac7f8a35b225eb5acad5827326026
Tsveta Brakalova <cookie.bra@gmail.com>
2015-06-08 14:48:56 +0300

    Added left-sidebar.html to hold left-sidebar stuff.

---
+++ src/app/common/categories/categoryResource.js
@@ -0,0 +1,7 @@
+app.factory('CategoryResource',['$resource', 'apiUrl', function($resource, apiUrl){
+  //var url = apiUrl + '/towns';
+
+  //var TownResource = $resource(url, {adId:'@id'}, { queryAll: {method: 'GET', isArray: false}} );
+
+  // return TownResource;
+}]);
