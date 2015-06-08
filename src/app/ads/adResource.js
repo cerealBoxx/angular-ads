9fb1aee17554d2298b476c422d786d95fbd252b6
Tsveta Brakalova <cookie.bra@gmail.com>
2015-06-08 10:45:46 +0300

    Changed login buttons for main page to primary.

---
+++ src/app/ads/adResource.js
@@ -0,0 +1,9 @@
+var adsServices = angular.module('adsServices', ['ngResource']);
+
+adsServices.factory('AdResource',['$resource', 'apiUrl', function ($resource, apiUrl) {
+      var url = apiUrl + '/ads/:id';
+
+      var AdResource = $resource(url, {adId:'@id'}, { queryAll: {method: 'GET', isArray: false}} );
+
+      return AdResource;
+  }]);
